a2ff1987ea3290c0712650247536748df66cee08
Premjeet Sahu <cs2054@global.org.in>
2024-03-13 18:29:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of Cities</p>} />
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/CityList.jsx
@@ -0,0 +1,6 @@
+import styles from "./CityList.module.css";
+function CityList() {
+  return <ul style={styles.cityList}></ul>;
+}
+
+export default CityList;
